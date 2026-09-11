1e126f50d3f16a3fc0f04530680a0e408d229225
jooeunhan <yeppijune@gmail.com>
2026-09-12 00:37:37 +0900

    :sparkles: Feat: 보상 가이드 페이지 ui 구현

--- src/pages/Reward/RewardGuidePage.jsx
+++ src/pages/Reward/RewardGuidePage.jsx
@@ -1,56 +1,86 @@
 import styled from "styled-components";
 import { useNavigate, useParams } from "react-router-dom";
 
-import GiftBox from "../../components/Reward/GiftBox";
+import FloatingGift from "../../components/Reward/FloatingGift";
+import PrimaryButton from "../../components/Button/PrimaryButton";
+
+import giftImage from "../../assets/images/gift_image.jpg";
 
 export default function RewardGuidePage() {
   const navigate = useNavigate();
   const { toilet_code } = useParams();
 
-  const handleGiftClick = () => {
+  const handleOpenGift = () => {
     navigate(`/${toilet_code}/survey`);
   };
 
   return (
     <Container>
-      <GiftBox onClick={handleGiftClick} />
+      <Content>
+        <FloatingGift src={giftImage} />
 
-      <Title>선물이 도착했어요!</Title>
+        <Title>두근두근 랜덤박스</Title>
 
-      <Description>
-        설문에 참여해주시면
-        <br />
-        랜덤 보상이 있어요.
-      </Description>
+        <Description>
+          설문 완료 후
+          <br />
+          랜덤박스를 개봉하세요!
+        </Description>
+
+        <ButtonArea>
+          <PrimaryButton onClick={handleOpenGift}>
+            랜덤박스 개봉하기
+          </PrimaryButton>
+        </ButtonArea>
+      </Content>
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled.main`
+  width: 100%;
+  min-height: 100%;
+
+  display: flex;
+  justify-content: center;
+
+  background: #ffffff;
+`;
+
+const Content = styled.section`
   width: 100%;
 
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  padding: 80px 24px 24px;
-  box-sizing: border-box;
+  padding: 170px 70px;
 `;
 
 const Title = styled.h1`
-  margin: 28px 0 10px;
+  margin: 36px 0 10px;
 
   font-size: 22px;
   font-weight: 700;
+
   color: #1e293b;
 `;
 
 const Description = styled.p`
   margin: 0;
 
-  font-size: 15px;
-  line-height: 1.6;
-  color: #64748b;
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: 400;
 
   text-align: center;
+
+  color: #64748b;
+`;
+
+const ButtonArea = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 36px;
 `;
